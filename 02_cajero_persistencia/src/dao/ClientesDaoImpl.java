1183aa0e59a5eb96dc54dcf6e0da3cd6b6de57af
Tardes <Tardes@A13E32.mshome.net>
2025-02-24 16:57:11 +0100

    1arreglo

--- 02_cajero_persistencia/src/dao/ClientesDaoImpl.java
+++ 02_cajero_persistencia/src/dao/ClientesDaoImpl.java
@@ -14,24 +14,22 @@ class ClientesDaoImpl implements ClientesDao {
 
 	@Override
 	public List<Cliente> findByCuenta(int idCuenta) {
-			List<Cliente> lista=new ArrayList<Cliente>();
-			try(Connection con=LocatorEntity.getConnection()) {
-				String sql="select clientes.* from clientes, titulares  where titulares.idCuenta = ? and titulares.idCliente = clientes.dni";
-				PreparedStatement ps = con.prepareStatement(sql);
-				ps.setInt(1, idCuenta);
-				ResultSet rs =ps.executeQuery();
-				while(rs.next()) {
-					lista.add(new Cliente(rs.getInt("dni"),
-							rs.getString("nombre"),
-							rs.getString("direccion"),
-							rs.getInt("telefono")));
-					
-				}
-				
-			}catch (SQLException ex) {
-				ex.printStackTrace();
+		List<Cliente> lista = new ArrayList<Cliente>();
+		try (Connection con = LocatorEntity.getEntityManager()) {
+			String sql = "select clientes.* from clientes, titulares  where titulares.idCuenta = ? and titulares.idCliente = clientes.dni";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, idCuenta);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				lista.add(new Cliente(rs.getInt("dni"), rs.getString("nombre"), rs.getString("direccion"),
+						rs.getInt("telefono")));
+
 			}
-			return lista;	
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return lista;
 	}
 
 }
